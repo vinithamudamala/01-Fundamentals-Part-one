446b980387332db47af8cd4de347a465b2bca956
vinithamudamala <mudamalavinithareddy@gmail.com>
2023-09-25 18:16:52 +0530

    add

--- starter/task1.js
+++ starter/task1.js
@@ -2,6 +2,7 @@
 -> show that array with then alert command.
 -> show how many elements the array contains.
 -> add 2,4,6,8 to the end of the array and show it again
+-> insert the value 20 on the 20th array element and show again
 */
 
 array = [1, 3, 5, 7, 9];
