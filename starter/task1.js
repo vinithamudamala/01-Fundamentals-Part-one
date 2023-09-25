372d905ecae5b048da3ada45a6955a2023d31303
vinithamudamala <mudamalavinithareddy@gmail.com>
2023-09-25 18:13:41 +0530

    add

---
+++ starter/task1.js
@@ -0,0 +1,11 @@
+/*create an array with all odd numbers that are smaller than 10
+-> show that array with then alert command.
+-> show how many elements the array contains.
+-> add 2,4,6,8 to the end of the array and show it again
+*/
+
+array = [1, 3, 5, 7, 9];
+alert(" show array with  odd numbers that are smaller than 10");
+array.push(2, 4, 6, 8);
+array.splice(19, 0, 20);
+console.log(array);
